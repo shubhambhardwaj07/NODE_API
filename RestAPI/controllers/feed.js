23c5ac3bbb16ac4e28de5ddd905972efd6843c8a
shubh <shubham998845@gmail.com>
2023-08-19 14:38:50 +0530

    post routed updated

--- RestAPI/controllers/feed.js
+++ RestAPI/controllers/feed.js
@@ -1,13 +1,41 @@
+const { validationResult } = require("express-validator/chack");
+
 exports.getPosts = (req, res, next) => {
-  res.status(200).json({ posts: [{ title: "posts", content: "new post" }] });
+  res.status(200).json({
+    posts: [
+      {
+        _id: "1",
+        title: "posts",
+        content: "new post",
+        imageUrl: "images/duck.jpg",
+        creator: { name: "shubh" },
+        createdAt: new Date(),
+      },
+    ],
+  });
 };
 
 exports.createPost = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res
+      .status(422)
+      .json({
+        message: "Validation Failed, entered data is incorrect",
+        errors: errors.array(),
+      });
+  }
   const title = req.body.title;
   const content = req.body.content;
   // create post in db
   res.status(201).json({
     message: "Post created successfully",
-    post: { id: new Date().toISOString(), title: title, content: content },
+    post: {
+      _id: new Date().toISOString(),
+      title: title,
+      content: content,
+      creator: { name: "shubh" },
+      createdAt: new Date(),
+    },
   });
 };
--- RestAPI/routes/feed.js
+++ RestAPI/routes/feed.js
@@ -1,9 +1,17 @@
 const express = require("express");
+const { body } = require("express-validator/check");
 const feedController = require("../controllers/feed");
 
 const router = express.Router();
 //feed/posts
 router.get("/posts", feedController.getPosts);
-router.post("/post", feedController.createPost);
+router.post(
+  "/post",
+  [
+    body("title").trim().isLength({ min: 5 }),
+    body("content").trim().islength({ min: 5 }),
+  ],
+  feedController.createPost
+);
 
 module.exports = router;
