4c5920978f40e34168a6efb06d33ba977f353956
shubh <shubham998845@gmail.com>
2023-08-20 13:13:52 +0530

    authentication with jwt

--- RestAPI/routes/feed.js
+++ RestAPI/routes/feed.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const { body } = require("express-validator");
 const feedController = require("../controllers/feed");
+const isAuth = require("../middleware/is-auth");
 
 const router = express.Router();
 //feed/posts
-router.get("/posts", feedController.getPosts);
+router.get("/posts", isAuth, feedController.getPosts);
 router.post(
   "/post",
   [body("title").trim(), body("content").trim()],
